a6036cb2d8850bbbd014aaf8b895c41089153810
Omar Blanchet <omar.blanchet@gmail.com>
2018-09-22 12:48:57 -0700

    step for day commit

--- src/test/java/edu/ucsd/cs/Main.java
+++ src/test/java/edu/ucsd/cs/Main.java
@@ -3,5 +3,7 @@ package edu.ucsd.cs;
 public class Main {
     public static void main(String[] args){
     //Quivole aqui andamos al 100 pariente
+        System.out.println(args[1]); // WGG - added line
+
     }
 }
