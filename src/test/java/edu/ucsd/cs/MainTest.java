5a1212a7a58db2ab8b011da811721e73a82e535f
Omar Blanchet <omar.blanchet@gmail.com>
2018-09-22 12:45:05 -0700

    main test commit

---
+++ src/test/java/edu/ucsd/cs/MainTest.java
@@ -0,0 +1,13 @@
+package edu.ucsd.cs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        Main.main(new String[]{"arg1", "arg2"});
+        assertTrue(true);
+
+    }
+}
