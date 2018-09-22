cded69f0113055f09f29d9a5e184afffacf81faa
FERMINALBERTO <fermin.armenta@cetys.mx>
2018-09-22 11:40:51 -0700

    Commit main

--- src/test/java/edu/ucsd/cs/Main.java
+++ src/test/java/edu/ucsd/cs/Main.java
@@ -3,5 +3,6 @@ package edu.ucsd.cs;
 public class Main {
     public static void main(String[] args){
     //Quivole aqui andamos al 100 pariente
+        System.out.println(args[0]); // WGG - added line
     }
 }
